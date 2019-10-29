144b4bfbe3f30c731f6dffd8f06f93a1eeeec5a7
James Feist <james.feist@linux.intel.com>
2019-10-29 15:18:43 -0700

    Update status in Redfish
    Create associations when failed to change the
    status to Warning.
    
    Tested:
    
    {
        "@odata.context": "/redfish/v1/$metadata#Drive.Drive",
        "@odata.id": "/redfish/v1/Systems/system/Storage/1/Drive/Drive_1",
        "@odata.type": "#Drive.v1_7_0.Drive",
        "Status": {
            "Health": "Warning",
            "HealthRollup": "Warning",
            "State": "Enabled"
        }
    }
    
    Change-Id: I3e5f87dc1253e8f24396a924e79d5474b0e5754a
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- hsbp-manager/include/utils.hpp
+++ hsbp-manager/include/utils.hpp
@@ -71,6 +71,12 @@ const static constexpr char* path = "/xyz/openbmc_project/state/host0";
 const static constexpr char* property = "CurrentHostState";
 } // namespace power
 
+namespace association
+{
+const static constexpr char* interface =
+    "xyz.openbmc_project.Association.Definitions";
+} // namespace association
+
 namespace hsbp
 {
 enum class registers : uint8_t
